39f550907c41200858e8c24023f5ff712301b578
felipe jarmillo <felipe.jaramillo@booktofly.com.co>
2021-11-15 15:52:39 -0500

    Add handle errors

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,12 +10,19 @@ export class HomeComponent {
 
   newReleases: any[] = [];
   loading: boolean;
+  errorService: boolean;
+  errorServiceMessage: string = '';
 
   constructor(private spotify: SpotifyService) { 
     this.loading = true;
+    this.errorService = false;
 
-    this.spotify.getNewReleases().subscribe( (data: any) => {
+    this.spotify.getNewReleases().subscribe((data: any) => {
       this.newReleases = data;
+      this.loading = false;
+    }, (errorResponse: any) => {
+      this.errorService = true;
+      this.errorServiceMessage =  errorResponse?.error?.error?.message;
       this.loading = false;
     });
   }
